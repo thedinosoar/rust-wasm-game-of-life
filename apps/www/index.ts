25307ae48a8430f7e31cd3e0dd18bb85db766254
Keegan McGonigle <31631874+thedinosoar@users.noreply.github.com>
2025-04-28 23:52:08 -0700

    converted to canvas

--- apps/www/index.ts
+++ apps/www/index.ts
@@ -1,16 +1,86 @@
-import { Universe } from "@repo/wasm-game-of-life";
+import { Universe, Cell } from "@repo/wasm-game-of-life";
+import { memory } from "@repo/wasm-game-of-life/wasm";
 
-const pre = document.getElementById("game-of-life-canvas");
-if (!pre) {
+const canvas = document.getElementById(
+  "game-of-life-canvas"
+) as HTMLCanvasElement;
+
+if (!canvas) {
   throw new Error("game-of-life-canvas not found");
 }
+
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
 const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext("2d");
+if (!ctx) {
+  throw new Error("Canvas ctx is null");
+}
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
 const renderLoop = () => {
-  pre.textContent = universe.render();
   universe.tick();
+
+  drawGrid();
+  drawCells();
 
   requestAnimationFrame(renderLoop);
 };
+
 requestAnimationFrame(renderLoop);
 // // wasm.greet("ayo");
 // console.log(wasm.Cell.Alive);
